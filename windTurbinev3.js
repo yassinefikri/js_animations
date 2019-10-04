c54585a9693f06a1ae8d0b7456ffb77a89ee1bc1
yassinefikri <49125227+yassinefikri@users.noreply.github.com>
2019-10-04 19:35:27 +0100

    Create windTurbinev3.js

---
+++ windTurbinev3.js
@@ -0,0 +1,212 @@
+/*-----------------------------------------------------------------------------|
+|                     WindTurbines Background Animation                        |
+|                           Author : Yassine Fikri                             |
+|               MIT license: http://opensource.org/licenses/MIT                |
+|          More Works : https://github.com/yassinefikri/js_animations          |
+------------------------------------------------------------------------------*/
+
+var grass_lng=5;
+var windTurbine_length= 250;
+var windTurbine_Speed= 1;
+var bgcolor="linear-gradient(180deg, rgba(0,0,50,1) 0%, rgba(0,100,250,1) 100%)";
+var g=0;
+var g2=100;
+var incg=1;
+var dirg=1;
+var sposx=[];
+var sposy=[];
+var nstars=20;
+
+var canvas= document.querySelector("canvas");
+var ww= document.body.clientWidth; 
+var wh= window.innerHeight;
+
+for(var i=0;i<nstars;i++){
+    sposx.push(parseInt(Math.random()*ww));
+    sposy.push(parseInt(Math.random()*wh/3));
+}
+
+//Setting Canvas Height & Width
+canvas.width= ww;
+canvas.height= wh;
+canvas.style.background=bgcolor;
+
+var c= canvas.getContext("2d");
+windTurbine_length= Math.min(2*wh/5,300);
+countWindTurbines= parseInt(ww/(windTurbine_length*4/3));
+
+function drawPay(){
+    c.beginPath();
+    c.fillStyle="rgba(0,"+(20+g/2)+",0,1)";
+    c.fillRect(0,7*wh/8,ww,wh);
+    for(var i=0;i<nstars;i++){
+        c.beginPath();
+        c.ellipse(sposx[i],sposy[i],(100-g2)/100,(100-g2)/100,0,0,Math.PI*2,true);
+        c.fillStyle="white";
+        c.fill();
+    }
+}
+
+function Grass(){
+    this.init= function(posx,posy){
+        this.posx=posx;
+        this.posy=posy;
+        this.opa=Math.random()*100;
+        this.dx=Math.random()*6 -3;
+        this.dir=1;
+    }
+    this.draw= function(){
+        c.beginPath();
+        c.moveTo(this.posx,this.posy);
+        c.lineTo(this.posx+this.dx,this.posy-grass_lng);
+        c.strokeStyle="rgba(0,"+(20+g/2)+",0,1)";
+        c.stroke();
+    }
+    this.update= function(){
+        if(this.dir==1){
+            this.dx+=0.2;
+        }
+        else{
+            this.dx-=0.2;
+        }
+        if(this.dx>=3){
+            this.dir=0;
+        }
+        if(this.dx<=-3){
+            this.dir=1;
+        }
+        this.draw();
+    }
+}
+
+function WindTurbine(){
+    this.init= function(posx,lng){
+        this.posx= posx;
+        this.ang=parseInt(Math.random()*360);
+        this.lng=lng;
+    }
+    this.draw= function(){
+        c.beginPath();
+        c.fillStyle= "rgba("+(150+g)+","+(150+g)+","+(150+g)+",1)";
+        c.fillRect(this.posx,7*wh/8-this.lng,5,this.lng);
+        c.beginPath();
+        c.ellipse(this.posx+2.5,7*wh/8-this.lng,6,6,0,0,2*Math.PI,true);
+        c.fill();
+        temp_ang=this.ang;
+        for(var i=0;i<3;i++){
+            c.beginPath();
+            c.moveTo(this.posx+2.5+Math.cos((temp_ang-37.5)*(Math.PI/180))*6,7*wh/8-this.lng+Math.sin((temp_ang-37.5)*(Math.PI/180))*6);
+            c.lineTo(this.posx+2.5+Math.cos((temp_ang+37.5)*(Math.PI/180))*6,7*wh/8-this.lng+Math.sin((temp_ang+37.5)*(Math.PI/180))*6);
+            c.lineTo(this.posx+2.5+Math.cos(temp_ang*(Math.PI/180))*this.lng/2,7*wh/8-this.lng+Math.sin(temp_ang*(Math.PI/180))*this.lng/2);
+            c.fill();
+            temp_ang+=120;
+        }
+    }
+    this.update= function(){
+        this.draw();
+        this.ang-=windTurbine_Speed;
+        if(this.ang==-1){
+            this.ang=359;
+        }
+    }
+}
+
+function changeBackground(){
+    bgcolor="linear-gradient(180deg, rgba(0,"+g+","+(50+2*g)+",1) 0%, rgba(0,"+g2+","+(50+2*g2)+",1) 100%)";
+    canvas.style.background= bgcolor;
+    if(incg==1){
+        g+=0.25;
+    }
+    else if(incg==2){
+        g-=0.25;
+    }
+    else if(incg==3){
+        g2-=0.25;
+    }
+    else{
+        g2+=0.25;
+    }
+    if(parseInt(g)==100){
+        incg=2;
+        dirg=0;
+    }
+    if(parseInt(g2)==0){
+        incg=0;
+        dirg=1;
+    }
+    if(parseInt(g)==0 || parseInt(g2)==100){
+        if(dirg==1){
+            incg=1;
+        }
+        else{
+            incg=3;
+        }
+    }
+}
+
+windTurbinesArr=[];
+for(var i=0;i<countWindTurbines;i++){
+    windTurbinesArr.push(new WindTurbine());
+    windTurbinesArr[i].init((i+1)*ww/(countWindTurbines+1),windTurbine_length);
+}
+grassArr= [];
+var grassCounter=0;
+for(var i=0;i<ww;i++){
+    grassArr.push(new Grass());
+    grassArr[grassCounter].init(i,7*wh/8);
+    grassCounter++;
+    grassArr.push(new Grass());
+    grassArr[grassCounter].init(i,7*wh/8+grass_lng/2);
+    grassCounter++;
+}
+
+function animate(){
+    requestAnimationFrame(animate);
+    c.clearRect(0,0,ww,wh);
+    drawPay();
+    for(var i=0;i<countWindTurbines;i++){
+        windTurbinesArr[i].update();
+    }
+    for(var i=0;i<grassCounter;i++){
+        grassArr[i].update();
+    }
+    changeBackground();
+}
+animate();
+
+function initCan(){
+    drawPay();
+    sposx=[];
+    sposy=[];
+    for(var i=0;i<nstars;i++){
+        sposx.push(parseInt(Math.random()*ww));
+        sposy.push(parseInt(Math.random()*wh/3));
+    }
+    windTurbine_length= Math.min(2*wh/5,300);
+    countWindTurbines= parseInt(ww/(windTurbine_length*4/3));
+    windTurbinesArr=[];
+    for(var i=0;i<countWindTurbines;i++){
+        windTurbinesArr.push(new WindTurbine());
+        windTurbinesArr[i].init((i+1)*ww/(countWindTurbines+1),windTurbine_length);
+    }
+    grassArr= [];
+    grassCounter=0;
+    for(var i=0;i<ww;i++){
+        grassArr.push(new Grass());
+        grassArr[grassCounter].init(i,7*wh/8);
+        grassCounter++;
+        grassArr.push(new Grass());
+        grassArr[grassCounter].init(i,7*wh/8+grass_lng/2);
+        grassCounter++;
+    }
+}
+
+//Resizing the Canvas when Resizing Window
+window.onresize = resize;
+function resize() {
+    ww= document.body.clientWidth; 
+    wh= window.innerHeight;
+    canvas.height=wh;
+    canvas.width=ww;
+    initCan();
+}
